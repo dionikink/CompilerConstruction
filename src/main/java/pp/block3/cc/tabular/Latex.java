f4957c169aef766401998b173ebfa3e7702ef40c
Jens2 <j.vdmeer10@hotmail.com>
2016-05-11 14:53:02 +0200

    fix

--- src/main/java/pp/block3/cc/tabular/Latex.java
+++ src/main/java/pp/block3/cc/tabular/Latex.java
@@ -2,10 +2,29 @@ package pp.block3.cc.tabular;
 
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by Dion on 11-5-2016.
+ *
  */
 public class Latex extends LatexBaseListener {
+
+    private PrintWriter writer;
+
+    public void init(String fileName) {
+        try {
+            this.writer = new PrintWriter(fileName + ".html", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void end() {
+        this.writer.close();
+    }
 
     @Override public void exitTable(LatexParser.TableContext ctx) {
 
@@ -26,5 +45,5 @@ public class Latex extends LatexBaseListener {
     @Override public void visitTerminal(TerminalNode node) {
 
     }
-
 }
+
